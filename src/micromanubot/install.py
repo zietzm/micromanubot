a012b28ffaf5743df869f6bb1e70f7ab14c67048
zietzm <michael.zietz@gmail.com>
2024-04-28 09:40:38 -0500

    fix: update tlmgr before installing packages

--- src/micromanubot/install.py
+++ src/micromanubot/install.py
@@ -56,11 +56,22 @@ def install_tinytex(root: pathlib.Path) -> None:
         target_folder=tex_dir, download_folder=tex_dir
     )
     check_pdflatex_bibtex_installed()
+    update_tlmgr()
     install_packages(DEFAULT_PACKAGES)
 
 
 def uninstall_tinytex(root: pathlib.Path) -> None:
     shutil.rmtree(root.joinpath("tinytex"))
+
+
+def update_tlmgr() -> None:
+    tlmgr = find_tlmgr()
+    result = subprocess.run([tlmgr.as_posix(), "update", "--self"], capture_output=True)
+    if result.returncode != 0:
+        raise RuntimeError(
+            f"Failed to update tlmgr: {result.stdout.decode()} "
+            f"{result.stderr.decode()}"
+        )
 
 
 def install_packages(packages: list[str]) -> None:
